bee988fda041c08611b77e948191e5a09a7915e1
Elena Kolesnikovich <solnechnaya_88@list.ru>
2024-07-29 23:34:41 +0200

    scr/app.js : added JSDOC

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
+/** 
+ * Function to calculate the total score of students
+ * @param {object} scores A list of students
+ * @param {string} scores.name A name of the student
+ * @param {number} scrores.score A student assessment
+ * @returns {number} Total score of students
+*/
 function getScore(scores){
-
   return Object.values(scores).reduce((accumulator, value) => accumulator + value);
 }
 
 const scores = {
   Anna: 10,
   Olga: 5,
-  Ivan: 3
+  Ivan: 3,
+  Peter: 7,
+  Mike: 13
 };
 const totalSum = getScore(scores); 
 console.log('totalSum : ', totalSum)
